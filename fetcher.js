54c0c2d2bf847a575693eef84b49913dc816a938
VktYe <v.yehorenko@gmail.com>
2025-03-24 23:26:19 -0300

    add edge cases if file already exist ask to overwrite?

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,68 @@
 const needle = require('needle');
-const fs = require('fs')
+const fs = require('fs');
+const readline = require("readline");
 const args = process.argv;
 const fetcher = args.slice(2);
 const url = fetcher[0];
 const path = fetcher[1];
 
-
-needle.get(url, (error, response, body) => {
-  if (error) {
-    console.log('Error: ', error)
-  }  
-  fs.writeFile(path, body, err => { // writes to a file 
-    if (err) {
-      console.log("Error in write file:", err)
-    }
-    const bytes = body.length;
-    console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
-      
-  })
-  
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+if (!url || !path) {
+  console.log("No URL or PATH provided");
+  process.exit(1);
+}
+
+
+// Check if the file exists in current directory.
+fs.access(path, fs.constants.F_OK, (err) => {
+  
+  if (!err) { // file exist - ask question
+    rl.question(`File ${path} already exists. To overwrite press Y:   `, (answer) => {
+      console.log('User answered:', answer);
+      if (answer.toLowerCase() === 'y') {
+        pageDownloader();
+      } else {
+        console.log("Skipping download. File is not overwritten");
+        rl.close();
+        process.exit(0);
+      }
+    });
+
+  } else { // file does not exist - download
+    pageDownloader();
+    
+  }
+});
+
+
+const pageDownloader = function() {
+  console.log('Starting download from:', url);
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      console.log("Error: ", error);
+      process.exit(1);
+    }
+    if (response && response.statusCode !== 200) {
+      console.log("Error. HTTP Status: ", response.statusCode);
+      rl.close();
+      process.exit(1);
+
+    }
+      fs.writeFile(path, body, err => { // writes to a file
+      if (err) {
+        console.log("Error in write file: ", err);
+        rl.close();
+        process.exit(1);
+
+      }
+      const bytes = body.length;
+      console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
+      rl.close();
+    });
+  
+  });
+};
